57b9933bf15fc34abe2c3b386214cea7e8ce0110
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2015-09-01 04:25:42 -0500

    Major refactor to make api easier

--- spec/lib/scrawl_spec.rb
+++ spec/lib/scrawl_spec.rb
@@ -1,189 +1,267 @@
 require "spec_helper"
 
 RSpec.describe Scrawl do
-  let(:input) { { a: 1 } }
-  let(:trees) { [input] }
-  let(:scrawl) { described_class.new(*trees) }
-  let(:internal_tree) { scrawl.instance_variable_get(:@tree) }
+  let(:inputs) { [input] }
+  let(:scrawl) { described_class.new(*inputs) }
 
-  describe ".new" do
-    context "with single" do
-      shared_examples "setup single" do
-        it "sets internal tree to that hash" do
-          expect(internal_tree).to eq(input)
-        end
-      end
-
-      context "hash object" do
-        include_examples "setup single"
-      end
-
-      context "scrawl object" do
-        let(:tree) { Scrawl.new(*trees) }
-
-        include_examples "setup single"
-      end
+  shared_context "for single input" do
+    let(:input) do
+      { "a" => "a" }
     end
 
-    context "with multiple" do
-      let(:merged_input) { { a: 1, b: 2 } }
-
-      shared_examples "setup all" do
-        it "merges all arguments into a single tree" do
-          expect(internal_tree).to eq(merged_input)
-        end
-      end
-
-      context "hashes objects" do
-        let(:trees) { [{ a: 1 }, { b: 2 }] }
-      end
-
-      context "scrawl objects" do
-        let(:trees) { [Scrawl.new(a: 1), Scrawl.new(b: 2)] }
-      end
+    let(:output) do
+      'a="a"'
     end
   end
 
-  describe "#merge" do
-    let(:merge) { scrawl.merge(compared) }
-
-    shared_examples "merges" do
-      it "combines with the internal tree" do
-        expect(merge).to eq(a: 1, b: 2)
-      end
+  shared_context "for multiple input" do
+    let(:input) do
+      { "a" => "a", "b" => "b" }
     end
 
-    context "with hash" do
-      let(:compared) { { b: 2 } }
-
-      include_examples "merges"
+    let(:output) do
+      'a="a" b="b"'
     end
+  end
 
-    context "with scrawl object" do
-      let(:compared) { Scrawl.new(b: 2) }
-
-      include_examples "merges"
+  shared_examples "for output" do
+    it "joins the key value together with an ="  do
+      expect(function).to eq(output)
     end
   end
 
   describe "#inspect" do
-    let(:stream) { "a=1" }
-    let(:namespace) { nil }
-    let(:inspect) { scrawl.inspect(namespace) }
+    let(:namespace) do
+      nil
+    end
+    let(:function) do
+      scrawl.inspect(namespace)
+    end
 
-    context "with a single pair" do
-      it "joins the key value together with an ="  do
-        expect(inspect).to eq(stream)
-      end
+    context "with single input" do
+      context "with a namespace" do
+        include_context "for single input"
 
-      context "and text value" do
-        let(:input) { { a: "example" } }
+        let(:namespace) do
+          "a"
+        end
+        let(:output) do
+          'a.a="a"'
+        end
 
-        it "includes the escaped text" do
-          expect(inspect).to eq("a=\"example\"")
+        it "combines the namespace keys" do
+          expect(function).to eq(output)
         end
       end
 
-      context "and a proc value" do
-        let(:input) { { a: -> { "example#{1 + 1}" } } }
-
-        it "evaluate the proc" do
-          expect(inspect).to eq("a=\"example2\"")
+      context "that is a string" do
+        let(:input) do
+          { "a" => "a" }
+        end
+        let(:output) do
+          'a="a"'
         end
 
-        context "with a random portion" do
-          let(:input) { { a: -> { "example#{rand(1..100000)}" } } }
-
-          it "never stores the value" do
-            first = scrawl.inspect
-            second = scrawl.inspect
-            expect(first).to_not eq(second)
-          end
-        end
+        include_examples "for output"
       end
 
-      context "and a number value" do
-        it "returns just the number" do
-          expect(inspect).to eq("a=1")
+      context "that is a nil" do
+        let(:input) do
+          { "a" => nil }
         end
+        let(:output) do
+          ''
+        end
+
+        include_examples "for output"
       end
 
-      context "and a symbol value" do
-        let(:input) { { a: :example } }
-
-        it "returns the escaped string equivelent" do
-          expect(inspect).to eq("a=\"example\"")
+      context "that is a numeric" do
+        let(:input) do
+          { "a" => 1 }
         end
+        let(:output) do
+          'a=1'
+        end
+
+        include_examples "for output"
       end
 
-      context "and a regexp value" do
-        let(:input) { { a: /foo(.+)/ } }
-
-        it "returns the escaped regexp" do
-          expect(inspect).to eq("a=/foo(.+)/")
+      context "that is a lambda" do
+        let(:input) do
+          { "a" => -> { "a" + "a" } }
         end
+        let(:output) do
+          'a="aa"'
+        end
+
+        include_examples "for output"
       end
 
-      context "and a hash" do
-        let(:input) { { a: Hash.new } }
-
-        it "returns as if nil" do
-          expect(inspect).to eq("a=nil")
+      context "that is an empty hash" do
+        let(:input) do
+          { "a" => {} }
         end
+        let(:output) do
+          ''
+        end
+
+        include_examples "for output"
+      end
+
+      context "that is a hash" do
+        let(:input) do
+          { "a" => { "a" => "a" } }
+        end
+        let(:output) do
+          'a.a="a"'
+        end
+
+        include_examples "for output"
+      end
+
+      context "that is a empty array" do
+        let(:input) do
+          { "a" => [] }
+        end
+        let(:output) do
+          ''
+        end
+
+        include_examples "for output"
+      end
+
+      context "that is a array" do
+        let(:input) do
+          { "a" => ["b", "c"] }
+        end
+        let(:output) do
+          'a.*="b" a.*="c"'
+        end
+
+        include_examples "for output"
+      end
+
+      context "that is a scrawl" do
+        let(:input) do
+          { "a" => Scrawl.new({ "a" => "a" }) }
+        end
+        let(:output) do
+          'a.a="a"'
+        end
+
+        include_examples "for output"
       end
     end
 
-    context "with multiple pairs" do
-      let(:input) { { a: 1, b: 2 } }
-      let(:stream) { "a=1 b=2" }
+    context "with multiple input" do
+      context "that are strings" do
+        let(:input) do
+          { "a" => "a", "b" => "b" }
+        end
+        let(:output) do
+          'a="a" b="b"'
+        end
 
-      it "delimits key value pairs with space" do
-        expect(inspect).to eq(stream)
+        include_examples "for output"
       end
-    end
 
-    context "with a given namespace" do
-      let(:namespace) { "b" }
-      let(:stream) { "b.a=1" }
+      context "that are nils" do
+        let(:input) do
+          { "a" => nil, "b" => nil }
+        end
+        let(:output) do
+          ''
+        end
 
-      it "prefixes the namespace to the key" do
-        expect(inspect).to eq(stream)
+        include_examples "for output"
       end
-    end
 
-    context "with nested hash tree" do
-      let(:input) { { a: { b: 1, c: 2 } } }
-      let(:stream) { "a.b=1 a.c=2" }
+      context "that are numerics" do
+        let(:input) do
+          { "a" => 1, "b" => 2 }
+        end
+        let(:output) do
+          'a=1 b=2'
+        end
 
-      it "namespaces the nested pairs" do
-        expect(inspect).to eq(stream)
+        include_examples "for output"
+      end
+
+      context "that are lambdas" do
+        let(:input) do
+          { "a" => -> { "a" + "a" }, "b" => -> { "b" + "b" } }
+        end
+        let(:output) do
+          'a="aa" b="bb"'
+        end
+
+        include_examples "for output"
+      end
+
+      context "that are empty hashs" do
+        let(:input) do
+          { "a" => {}, "b" => {} }
+        end
+        let(:output) do
+          ''
+        end
+
+        include_examples "for output"
+      end
+
+      context "that are hashs" do
+        let(:input) do
+          { "a" => { "a" => "a" }, "b" => { "b" => "b" } }
+        end
+        let(:output) do
+          'a.a="a" b.b="b"'
+        end
+
+        include_examples "for output"
+      end
+
+      context "that are empty arrays" do
+        let(:input) do
+          { "a" => [], "b" => [] }
+        end
+        let(:output) do
+          ''
+        end
+
+        include_examples "for output"
+      end
+
+      context "that are arrays" do
+        let(:input) do
+          { "a" => ["b", "c"], "b" => ["e", "f"] }
+        end
+        let(:output) do
+          'a.*="b" a.*="c" b.*="e" b.*="f"'
+        end
+
+        include_examples "for output"
+      end
+
+      context "that are scrawls" do
+        let(:input) do
+          { "a" => Scrawl.new({ "a" => "a" }), "b" => Scrawl.new({ "b" => "b" }) }
+        end
+        let(:output) do
+          'a.a="a" b.b="b"'
+        end
+
+        include_examples "for output"
       end
     end
   end
 
-  describe "#tree" do
-    let(:tree) { scrawl.tree }
-
-    it "returns the internal tree" do
-      expect(tree).to eq(internal_tree)
+  describe "#to_s" do
+    include_context "for single input"
+    it "uses inspect with no namespace" do
+      expect(scrawl).to receive(:inspect).with(nil)
+      scrawl.to_s
     end
   end
 
-  describe "#to_h" do
-    let(:to_h) { scrawl.to_h }
-
-    it "returns the internal tree" do
-      expect(to_h).to eq(internal_tree)
-    end
-  end
-
-  describe "#to_hash" do
-    let(:to_hash) { scrawl.to_hash }
-
-    it "returns the internal tree" do
-      expect(to_hash).to eq(internal_tree)
-    end
-  end
-
-  describe "#=="
 end
